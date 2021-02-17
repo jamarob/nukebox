66468845c7f5036ca0660c06baee4a50857c3e3b
Jan <janmartinrobert@gmail.com>
2021-02-17 12:28:29 +0100

    ♻️ Render TrackItems form static array

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,41 @@ import TrackItem from "../components/TrackItem";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const tracks = [
+    {
+      imgSrc:
+        "https://images.unsplash.com/photo-1613473060226-dd81153a63db?ixid=XwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      title: "Paul is Dead",
+      artist: "Scooter",
+    },
+    {
+      imgSrc:
+        "https://images.unsplash.com/photo-1593642532842-98d0fd5ebc1a?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      title: "The Unforgiven",
+      artist: "Metallica",
+    },
+    {
+      imgSrc:
+        "https://images.unsplash.com/photo-1612831457732-0f6b2156b92d?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      title: "Killing in the name of",
+      artist: "RATM",
+    },
+    {
+      imgSrc:
+        "https://images.unsplash.com/photo-1613467678197-6054e47c5fc0?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      title: "El Presidente",
+      artist: "Marteria",
+    },
+  ];
+  const trackItems = tracks.map((track) => (
+    <TrackItem
+      key={`${track.artist}-${track.title}`}
+      imgSrc={track.imgSrc}
+      artist={track.artist}
+      title={track.title}
+    />
+  ));
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,28 +46,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Greeting name="Jan" />
-      <ul className={styles.list}>
-        <TrackItem
-          imgSrc="https://images.unsplash.com/photo-1613473060226-dd81153a63db?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          title="Paul is Dead"
-          artist="Scooter"
-        />
-        <TrackItem
-          imgSrc="https://images.unsplash.com/photo-1593642532842-98d0fd5ebc1a?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          title="The Unforgiven"
-          artist="Metallica"
-        />
-        <TrackItem
-          imgSrc="https://images.unsplash.com/photo-1612831457732-0f6b2156b92d?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          title="Killing in the name of"
-          artist="RATM"
-        />
-        <TrackItem
-          imgSrc="https://images.unsplash.com/photo-1613467678197-6054e47c5fc0?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          title="El Presidente"
-          artist="Marteria"
-        />
-      </ul>
+      <ul className={styles.list}>{trackItems}</ul>
     </div>
   );
 }
