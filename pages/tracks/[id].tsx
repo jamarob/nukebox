aed29bdce2935e9b2663b47fee41e1b0f985eaa6
lmachens <leon@machens.koeln>
2021-02-18 17:09:28 +0100

    ✨ Display track details

--- pages/tracks/[id].tsx
+++ pages/tracks/[id].tsx
@@ -1,20 +1,35 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { APITrack } from "../../utils/api";
+import TrackDetails from "../../components/TrackDetails";
+import { APITrack, getTrack } from "../../utils/api";
 
 export default function Track() {
   const router = useRouter();
   const { id } = router.query;
-
   const [track, setTrack] = useState<APITrack>(null);
 
   useEffect(() => {
-    // todo get track by `id`
-  }, []);
+    if (typeof id !== "string") {
+      return;
+    }
+    getTrack(id).then((newTrack) => setTrack(newTrack));
+  }, [id]);
 
   if (!track) {
     return <div>Loading...</div>;
   }
 
-  return <div>Title: {track.title}</div>;
+  return (
+    <div>
+      {/* <header><Navigation /></header> */}
+      <main>
+        <TrackDetails
+          imgSrc={track.imgSrc}
+          title={track.title}
+          artist={track.artist}
+        />
+      </main>
+      {/* <footer><AudioPlayer /></footer> */}
+    </div>
+  );
 }
