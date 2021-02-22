22251ef6bf25fdacb596ad5856829e472fcd54df
lmachens <leon@machens.koeln>
2021-02-22 14:57:10 +0100

    ✨ Add play/pause

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "../styles/AudioPlayer.module.css";
+
 type Props = {
   src: string;
 };
 export default function AudioPlayer({ src }: Props) {
-  return <audio controls src={src} />;
+  const audioRef = useRef(new Audio(src));
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const audioElement = audioRef.current;
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioElement.play();
+    } else {
+      audioElement.pause();
+    }
+  }, [isPlaying]);
+
+  return (
+    <button className={styles.btn} onClick={() => setIsPlaying(!isPlaying)}>
+      <img src={isPlaying ? "/pause.svg" : "/play.svg"} />
+    </button>
+  );
 }
