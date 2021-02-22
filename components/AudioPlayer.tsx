2825c6897c2cc6f86855b22783206b595e1cc67c
lmachens <leon@machens.koeln>
2021-02-22 15:28:37 +0100

    💄 Style player

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -6,21 +6,36 @@ type Props = {
 };
 export default function AudioPlayer({ src }: Props) {
   const audioRef = useRef(new Audio(src));
+  const intervalRef = useRef<NodeJS.Timeout>();
   const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const audioElement = audioRef.current;
 
   useEffect(() => {
     if (isPlaying) {
       audioElement.play();
+      intervalRef.current = setInterval(() => {
+        setProgress(audioElement.currentTime);
+      }, 2000);
     } else {
+      clearInterval(intervalRef.current);
       audioElement.pause();
     }
   }, [isPlaying]);
 
   return (
-    <button className={styles.btn} onClick={() => setIsPlaying(!isPlaying)}>
-      <img src={isPlaying ? "/pause.svg" : "/play.svg"} />
-    </button>
+    <div className={styles.audioPlayer}>
+      <input
+        className={styles.duration}
+        type="range"
+        min="0"
+        max={audioElement.duration}
+        value={progress}
+      />
+      <button className={styles.btn} onClick={() => setIsPlaying(!isPlaying)}>
+        <img src={isPlaying ? "/pause.svg" : "/play.svg"} />
+      </button>
+    </div>
   );
 }
