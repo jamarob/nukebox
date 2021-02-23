cb7527cebea27bf55c88ae25416178ced5e78383
lmachens <leon@machens.koeln>
2021-02-23 12:26:57 +0100

    ✨ Add RESTful  endpoints

--- pages/api/tracks/index.ts
+++ pages/api/tracks/index.ts
@@ -1,7 +1,41 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
-import db from "../../../server/db.json";
+import { readDb, writeDb } from "../../../server/db";
+import { APITrack } from "../../../utils/api";
 
-export default (_req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(db.tracks);
+type Methods = {
+  [method: string]: (req: NextApiRequest, res: NextApiResponse) => void;
 };
+const methods: Methods = {
+  GET: handleGet,
+  POST: handlePost,
+};
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const method = methods[req.method];
+  if (!method) {
+    return res.status(405).json({
+      status: 405,
+      error: "Method not allowed",
+    });
+  }
+  await method(req, res);
+};
+
+async function handleGet(req: NextApiRequest, res: NextApiResponse) {
+  const db = await readDb();
+  res.status(200).json(db.tracks);
+}
+
+async function handlePost(req: NextApiRequest, res: NextApiResponse) {
+  const newTrack: APITrack = req.body;
+  const db = await readDb();
+  const trackExists = db.tracks.some((track) => track.id === newTrack.id);
+  if (trackExists) {
+    return res.status(409).json({
+      status: 409,
+      error: "Track already exists",
+    });
+  }
+  db.tracks.push(newTrack);
+  await writeDb(db);
+  res.status(201).json(newTrack);
+}
