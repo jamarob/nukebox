11eb038d412bfed4bec266d0a44525df6e9d2a9f
phlgr <philipp@gartz.dev>
2021-02-24 15:28:21 +0100

    🏷️ Add types to useLocalStorage

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
-export default function useLocalStorage(key: string, initialValue) {
-  const readFromLocalStorage = () => {
+export default function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T) => void] {
+  const readFromLocalStorage = (): T => {
     try {
       const item = localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
@@ -11,13 +14,13 @@ export default function useLocalStorage(key: string, initialValue) {
     }
   };
 
-  const writeToLocalStorage = (value) => {
+  const writeToLocalStorage = (value: T) => {
     localStorage.setItem(key, JSON.stringify(value));
   };
 
-  const [storedValue, setStoredValue] = useState(readFromLocalStorage);
+  const [storedValue, setStoredValue] = useState<T>(readFromLocalStorage);
 
-  const setValue = (value) => {
+  const setValue = (value: T) => {
     writeToLocalStorage(value);
     setStoredValue(value);
   };
